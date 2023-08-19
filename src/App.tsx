3e8b77dc2ccfef8e97098a73a5fa632521fbf0ac
Сергей <sergbfmv@mail.ru>
2023-08-19 17:54:01 +0300

    adding first styles and templates

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,43 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+
+const App = () => {
+    return (
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img
+                    src="https://cdn.logo.com/hotlink-ok/logo-social.png"
+                    alt="Company logo"/>
+            </header>
+            <nav className='nav'>
+                <div><a href="">Profile</a></div>
+                <div><a href="">Messages</a></div>
+                <div><a href="">News</a></div>
+                <div><a href="">Music</a></div>
+                <div><a href="">Settings</a></div>
+            </nav>
+            <main className='main'>
+                <div>
+                    <img src="https://www.ens-abidjan.org/slide/Jssor.Slider.FullPack/img/home/01.jpg" alt="bg-image"/>
+                </div>
+                <div>
+                    ava+sescription
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div>post 1</div>
+                        <div>post 2</div>
+                    </div>
+                </div>
+            </main>
+        </div>
+    );
 }
+
 
 export default App;
